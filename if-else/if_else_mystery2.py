00140e2f83e8117b0d6f85463fc22e41c15bce8f
Yağmur Taze <56199172+YagmurTaze@users.noreply.github.com>
2023-02-22 18:40:04 +0300

    Create if_else_mystery2.py

---
+++ if-else/if_else_mystery2.py
@@ -0,0 +1,9 @@
+"""
+ question	(your answer)	result:
+	line 1	    5 6	        pass
+	line 2	    6 3	        pass
+	line 3	    6 9	        pass
+	line 4	    18 9	      pass
+	line 5	    2 1	        pass
+
+"""
