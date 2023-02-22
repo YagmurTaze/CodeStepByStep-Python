ada4dcff1779bf516cec6a5ba3238144117578f8
Yağmur Taze <56199172+YagmurTaze@users.noreply.github.com>
2023-02-22 22:35:47 +0300

    Create assertions_2.py

---
+++ if-else/assertions_2.py
@@ -0,0 +1,8 @@
+"""
+Point	  n > b	        a > 1	        b > a
+  A	  SOMETIMES	    SOMETIMES	    SOMETIMES
+  B	  ALWAYS	      SOMETIMES	    SOMETIMES
+  C	  SOMETIMES	    ALWAYS	      ALWAYS
+  D	  SOMETIMES	    ALWAYS	      NEVER
+  E	  NEVER	        SOMETIMES	    SOMETIMES
+"""
