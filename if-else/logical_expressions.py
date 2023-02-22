73efb3138129bf20c885bd6487c8931171f5cfe4
Yağmur Taze <56199172+YagmurTaze@users.noreply.github.com>
2023-02-22 19:51:48 +0300

    Create logical_expressions.py

---
+++ if-else/logical_expressions.py
@@ -0,0 +1,15 @@
+"""
+ question	                                (your answer)	  result:
+	not b	                                      True	        pass
+	b or True	                                  True	        pass
+	(x > y) and (y > z)	                        False	        pass
+	(x == y) or (x <= z)	                      True	        pass
+	not (x % 2 == 0)	                          True	        pass
+	(x % 2 != 0) and b	                        False	        pass
+	b and not b	                                False	        pass
+	b or not b	                                True	        pass
+	(x < y) == b	                              True	        pass
+	not (x / 2 == 13) or b or (z * 3 == 96)	    True	        pass
+	(z < x) == False	                          True	        pass
+	not ((x > 0) and (y < 0))	                  False	        pass
+"""
