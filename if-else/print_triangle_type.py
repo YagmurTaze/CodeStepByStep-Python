d6c556241fa36fd164acc097efc1a5ad374868fa
Yağmur Taze <56199172+YagmurTaze@users.noreply.github.com>
2023-02-22 21:09:37 +0300

    Create print_triangle_type.py

---
+++ if-else/print_triangle_type.py
@@ -0,0 +1,9 @@
+"""
+question #1:
+  Which of the following are precondition(s) that are appropriate? Check all that apply.
+your answer:
+  No side's length may exceed the sum of any two other side lengths.
+  The three side lengths must be positive integers.
+result:
+  pass
+"""
