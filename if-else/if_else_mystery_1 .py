0de64e51790777f60d65d84bfbd78ec74fa12885
Yağmur Taze <56199172+YagmurTaze@users.noreply.github.com>
2023-02-22 20:25:56 +0300

    Update if_else_mystery_1 .py

--- if-else/if_else_mystery_1 .py
+++ if-else/if_else_mystery_1 .py
@@ -1,16 +1,15 @@
-# invalid syntax
-
 def if_else_mystery_1(x, y):
     z = 4
-    z = x + 1
-    if z <= y:
-        y += 1
+    if z <= x:
+        z = x + 1
     else:
         z = z + 9
+    if z <= y:
+        y += 1
     print(z, y)
     
    """
-   if_else_mystery_1(3, 20) = 4 21
+   if_else_mystery_1(3, 20) = 13 21
    if_else_mystery_1(4, 5)  = 5 6
    if_else_mystery_1(5, 5)  = 15 5
    if_else_mystery_1(6, 10) = 7 11
