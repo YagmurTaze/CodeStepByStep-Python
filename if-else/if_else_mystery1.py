4661264d2789d70878896c3e4601f3f056c8e07a
Yağmur Taze <56199172+YagmurTaze@users.noreply.github.com>
2023-02-22 18:34:02 +0300

    Update if_else_mystery1.py

--- if-else/if_else_mystery1.py
+++ if-else/if_else_mystery1.py
@@ -2,7 +2,7 @@
  question	(your answer)	result:
 
 	line 1	   -15	        pass
-	line 2	    6	          pass
+	line 2	    6	        pass
 	line 3	    10	        pass
 	line 4	    22	        pass
 	line 5	    52	        pass
