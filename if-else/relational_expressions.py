22196e6a0c762b9adf02c0406f2b4699d21af481
Yağmur Taze <56199172+YagmurTaze@users.noreply.github.com>
2023-02-22 22:49:33 +0300

    Create relational_expressions.py

---
+++ if-else/relational_expressions.py
@@ -0,0 +1,12 @@
+"""
+#	question	              (your answer) result
+1	x == 4	                        True	 pass
+2	x == y	                        False	 pass
+3	x == z	                        True	 pass
+4	y == z	                        False	 pass
+5	x + y > 0	                      True	 pass
+6	x - z != 0	                    False	 pass
+7	y * y <= z	                    False	 pass
+8	y // y == 1	                    True	 pass
+9	x * (y + 2) > y - (y + z) * 2	  True	 pass
+"""
