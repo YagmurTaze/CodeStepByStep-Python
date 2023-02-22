90d8d474ae66300d5e0af3f19de5d9c6b0609b11
Yağmur Taze <56199172+YagmurTaze@users.noreply.github.com>
2023-02-22 19:31:28 +0300

    Create input_color.py

---
+++ if-else/input_color.py
@@ -0,0 +1,9 @@
+txt = input("What color do you want? ")
+if txt == "R" or txt == "r":
+    print("You have chosen Red.")
+elif txt == "G" or txt == "g":
+    print("You have chosen Green.")
+elif txt == "B" or txt == "b":
+    print("You have chosen Blue.")
+else:
+    print("Unknown color:",txt)
