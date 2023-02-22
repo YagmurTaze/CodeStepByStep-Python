41f7054ecc214ebb93cec0306050cca85664a44d
Yağmur Taze <56199172+YagmurTaze@users.noreply.github.com>
2023-02-22 22:05:53 +0300

    Create percentage_grade.py

---
+++ if-else/percentage_grade.py
@@ -0,0 +1,11 @@
+percent = int(input("What percentage did you earn? "))
+if percent >= 90:
+    print("You got an A!")
+elif percent >= 80:
+    print("You got a B!")
+elif percent >= 70:
+    print("You got a C!")
+elif percent >= 60:
+    print("You got a D!")
+elif percent < 60:
+    print("You got an F!")
