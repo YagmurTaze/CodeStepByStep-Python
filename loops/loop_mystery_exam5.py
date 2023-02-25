f371d5f5012fb1d684a64e6fe2b0b9f4b0d1ae6c
Yağmur Taze <56199172+YagmurTaze@users.noreply.github.com>
2023-02-25 20:41:48 +0300

    Create loop_mystery_exam5.py

---
+++ loops/loop_mystery_exam5.py
@@ -0,0 +1,5 @@
+"""
+1	loop_mystery_exam5(12, 4)	(12 4) (8 5) 4	                pass
+2	loop_mystery_exam5(14, 2)	(14 2) (12 3) (9 4) 2	          pass
+3	loop_mystery_exam5(27, 3)	(27 3) (24 4) (20 5) (15 6) 3	  pass
+"""
