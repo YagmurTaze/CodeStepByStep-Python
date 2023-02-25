7ef971748e3e87c7fbeba063b2d8230683940dcb
Yağmur Taze <56199172+YagmurTaze@users.noreply.github.com>
2023-02-25 20:36:16 +0300

    Create loop_mystery_exam3.py

---
+++ loops/loop_mystery_exam3.py
@@ -0,0 +1,8 @@
+"""
+1	loop_mystery_exam3(7, 5) output	2 4 -2 3 3	                  pass
+2	loop_mystery_exam3(7, 5) return	12	                          pass
+3	loop_mystery_exam3(20, 4) output	16 3 13 2 11 1 10 0 0	      pass
+4	loop_mystery_exam3(20, 4) return	24	                        pass
+5	loop_mystery_exam3(40, 10) output	30 9 21 8 13 7 6 6 0 5 5	  pass
+6	loop_mystery_exam3(40, 10) return	50	                        pass
+"""
