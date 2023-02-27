6dc92082aa343fec4a535b7bbf96e47832d87d31
Yağmur Taze <56199172+YagmurTaze@users.noreply.github.com>
2023-02-27 23:26:59 +0300

    Update book.py

--- loops/book.py
+++ loops/book.py
@@ -1,26 +1,13 @@
-def plus_line():
-    print("+------------------------------+",end="")
-
-def py():
-    print("|    How to Code in Python     |",end="")
-
 def space(num):
     print(" " * num,end="")
     
-def slash(num):
-    print("/" * num)
-     
-def us_slash_3():
-    print("___/",end="")
+def plus_line():
+    print("+",end="")
+    print("-" * 30,end="")
+    print("+",end="")
 
-def us_slash_2(num):
-    print("__/" * num ,end="")
-    
-def main():
+def part1():
     num = 0
-    print("           ",end="")
-    plus_line()
-    print()
     for i in range (1,11):
         space(11 - i)
         print("/",end="")
@@ -29,15 +16,40 @@ def main():
         us_slash_3()
         us_slash_2(i-1)
         slash(i-1)
-    plus_line()
-    slash(10)
+        
+def part2():
     num = 0
     for i in range (5):
         py()
         slash(10 - num)
         num += 2
+
+def py():
+    str = "How to Code in Python "
+    print("|" + " " * 4 + str + " " * 4 + "|", end = "")
+        
+def slash(num):
+    print("/" * num)
+     
+def us_slash_3():
+    print("___/",end="")
+
+def us_slash_2(num):
+    print("__/" * num ,end="")
+
+def price():
+    print("Now only $50!")
+    
+def main():
+    space(11)
     plus_line()
     print()
-    print("Now only $50!")
+    part1()
+    plus_line()
+    slash(10)
+    part2()
+    plus_line()
+    print()
+    price()
     
 main()
