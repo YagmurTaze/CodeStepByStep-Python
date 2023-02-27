1a6eb58a71d40d6f171905def4569cd84401f71e
Yağmur Taze <56199172+YagmurTaze@users.noreply.github.com>
2023-02-27 23:23:53 +0300

    Update book2.py

--- loops/book2.py
+++ loops/book2.py
@@ -1,19 +1,33 @@
+def space(num):
+    print(" " * num,end="")
+    
 def plus_line():
     print("+",end="")
     print("-" * (SIZE * 3),end="")
     print("+",end="")
 
+def part1():
+    num = 0
+    for i in range (1,SIZE + 1):
+        space((SIZE + 1) - i)
+        print("/",end="")
+        space(SIZE * 3 - 3 - num)
+        num += 3
+        us_slash_3()
+        us_slash_2(i-1)
+        slash(i-1)
+        
+def part2():
+    num = 0
+    for i in range (int(SIZE / 2)):
+        py()
+        slash(SIZE - num)
+        num += 2
+
 def py():
     str = "How to Code in Python "
-    print("|",end="")
-    print(" " * int((( 3*SIZE ) - len(str)) / 2) ,end="")
-    print(str,end="")
-    print(" " * int((( 3*SIZE ) - len(str)) / 2) ,end="")
-    print("|",end="")
-
-def space(num):
-    print(" " * num,end="")
-    
+    print("|" + " " * int((( 3*SIZE ) - len(str)) / 2) + str + " " * int((( 3*SIZE ) - len(str)) / 2)  + "|", end = "")
+        
 def slash(num):
     print("/" * num)
      
@@ -27,25 +41,13 @@ def price():
     print("Now only $" + str(SIZE * 5) + "!")
     
 def main():
-    num = 0
-    print(" " * (SIZE + 1),end="")
+    space(SIZE + 1)
     plus_line()
     print()
-    for i in range (1,SIZE + 1):
-        space((SIZE + 1) - i)
-        print("/",end="")
-        space(SIZE * 3 - 3 - num)
-        num += 3
-        us_slash_3()
-        us_slash_2(i-1)
-        slash(i-1)
+    part1()
     plus_line()
     slash(SIZE)
-    num = 0
-    for i in range (int(SIZE / 2)):
-        py()
-        slash(SIZE - num)
-        num += 2
+    part2()
     plus_line()
     print()
     price()
