37bab7ce1fedc6eda6e41fdda43f230dd0fedd8a
Yağmur Taze <56199172+YagmurTaze@users.noreply.github.com>
2023-03-03 22:13:27 +0300

    Create while_loops.py

---
+++ loops/while_loops.py
@@ -0,0 +1,11 @@
+"""
+
+#	question	your answer	   result
+1	a.	          10	        pass
+2	b.	          zero	      pass
+3	c.	          infinity	  pass
+4	d.	          3	          pass
+5	e.	          5	          pass
+6	f.	          7	          pass
+
+"""
