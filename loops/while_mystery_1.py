3ca8df23413b195555221594db97b8ebf3c47155
Yağmur Taze <56199172+YagmurTaze@users.noreply.github.com>
2023-03-03 22:18:28 +0300

    Create  while_mystery_1.py

---
+++ loops/while_mystery_1.py
@@ -0,0 +1,9 @@
+"""
+#	question	    your answer	  result
+1	mystery(1)	    1 0	        pass
+2	mystery(6)	    4 2	        pass
+3	mystery(19)   	16 4	      pass
+4	mystery(39)	    32 5	      pass
+5	mystery(74)	    64 6	      pass
+
+"""
