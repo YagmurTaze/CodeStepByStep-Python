2b942525a25650835bc87c9b3c466dd674bd2377
Yağmur Taze <56199172+YagmurTaze@users.noreply.github.com>
2023-03-03 15:12:43 +0300

    Create print_wave.py

---
+++ loops/print_wave.py
@@ -0,0 +1,8 @@
+def print_wave():
+    for i in range (1, 5):
+        print("v" * i)
+        print("v" * i)
+    print("vvvvv")
+    for i in range (1, 5):
+        print("v" * (5-i))
+        print("v" * (5-i))
