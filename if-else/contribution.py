436cc2e3c817042eade5c2aa06e6aa37512aebe0
Yağmur Taze <56199172+YagmurTaze@users.noreply.github.com>
2024-08-22 13:16:33 +0300

    Update contribution.py

--- if-else/contribution.py
+++ if-else/contribution.py
@@ -1,14 +1,11 @@
 def contribution(count1, count2, sum, total):
-    sum = 1000
     times = int(input("Is your money multiplied 1 or 2 times? "))
     donation = int(input("And how much are you contributing? "))
     
     if times == 1:
         sum += donation
         count1 += 1
-
-    if times == 2:
+    elif times == 2:
         sum += 2 * donation
         count2 += 1
-        
     total += donation
